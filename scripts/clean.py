2ef7c8a08c82c1386009777b5e34d42cf3b19a3b
Will <wahubsch@gmail.com>
2024-01-28 06:13:37 -0500

    Fixing broken abbrs function, moving all into run function

--- scripts/clean.py
+++ scripts/clean.py
@@ -70,17 +70,19 @@ def direct_expand(match: regex.Match) -> str:
 
 # pre-compile regex for speed
 abbr_join = "|".join((name_expand | street_expand).keys())
-abbr_join_comp = regex.compile(rf"(\b(?:{abbr_join})\b\.?)")
-
-dir_fill = "|".join([r"\.?".join(list(abbr)) for abbr in direction_expand.keys()])
-dir_fill_comp = regex.compile(
-    rf"(?<!(?:^(?:Avenue|Street) |\.))(\b(?:{dir_fill})\b\.?)(?! (?:Street|Avenue))",
+abbr_join_comp = regex.compile(
+    rf"(\b(?:{abbr_join})\b\.?)",
     flags=regex.IGNORECASE,
 )
 
-sr_join = "|".join(street_expand.values())
+dir_fill = "|".join([r"\.?".join(list(abbr)) for abbr in direction_expand.keys()])
+dir_fill_comp = regex.compile(
+    rf"(?<!(?:^(?:Avenue|Street) |\.))(\b(?:{dir_fill})\b\.?)(?!(?:\.?[a-zA-Z]| (?:Street|Avenue)))",
+    flags=regex.IGNORECASE,
+)
+
 sr_comp = regex.compile(
-    rf"(?<!(?:^(?:{sr_join}).*))(\b(SR)\b\.?)(?!.*(?:{sr_join}))",
+    r"(\bS\.?R\b\.?)(?= [0-9]+)",
     flags=regex.IGNORECASE,
 )
 
@@ -109,162 +111,174 @@ def print_value(action: str, file: str, brand: str, items: int):
     print(f"{action.title() + '...':<16}{file.split('/')[-1]:<34}{brand:<30}{items:<6}")
 
 
-for file in files:
-    with open(file, "r") as f:
-        contents: dict = json.load(f)
+def run() -> None:
+    for file in files:
+        with open(file, "r") as f:
+            contents: dict = json.load(f)
 
-    features = contents["features"]
-    clean_data = {
-        "version": version,
-        "datetime": str(datetime.datetime.now().date()),
-    }
-    if "dataset_attributes" in contents:
-        try:
-            if contents["dataset_attributes"]["cleaning"]["version"] == version:
-                print_value(
-                    "skipping",
-                    file,
-                    features[0]["properties"].get("brand"),
-                    len(features),
-                )
-                continue
-        except KeyError:
-            pass
-        print_value(
-            "processing", file, features[0]["properties"].get("brand"), len(features)
-        )
-        contents["dataset_attributes"]["cleaning"] = clean_data
-    else:
-        contents["dataset_attributes"] = {"cleaning": clean_data}
+        features = contents["features"]
+        clean_data = {
+            "version": version,
+            "datetime": str(datetime.datetime.now().date()),
+        }
+        if "dataset_attributes" in contents:
+            try:
+                if contents["dataset_attributes"]["cleaning"]["version"] == version:
+                    print_value(
+                        "skipping",
+                        file,
+                        features[0]["properties"].get("brand"),
+                        len(features),
+                    )
+                    continue
+            except KeyError:
+                pass
+            print_value(
+                "processing",
+                file,
+                features[0]["properties"].get("brand"),
+                len(features),
+            )
+            contents["dataset_attributes"]["cleaning"] = clean_data
+        else:
+            contents["dataset_attributes"] = {"cleaning": clean_data}
 
-    wipe_repeat_tags: list[str] = []
-    feature_list: list[dict[str, str]] = [feature["properties"] for feature in features]
-    for repeat_tag in repeat_tags:
-        if all_the_same(features, repeat_tag) and any(
-            feature.get(repeat_tag) for feature in features
-        ):
-            wipe_repeat_tags.append(repeat_tag)
+        wipe_repeat_tags: list[str] = []
+        feature_list: list[dict[str, str]] = [
+            feature["properties"] for feature in features
+        ]
+        for repeat_tag in repeat_tags:
+            if all_the_same(features, repeat_tag) and any(
+                feature.get(repeat_tag) for feature in features
+            ):
+                wipe_repeat_tags.append(repeat_tag)
 
-    for obj in contents["features"]:
-        objt: dict[str, str] = obj["properties"]
-        # remove useless ATP-generated tags
-        for tag in useless_tags + wipe_repeat_tags:
-            objt.pop(tag, None)
+        for obj in contents["features"]:
+            objt: dict[str, str] = obj["properties"]
+            # remove useless ATP-generated tags
+            for tag in useless_tags + wipe_repeat_tags:
+                objt.pop(tag, None)
 
-        for name_tag in ["name", "branch"]:
-            if name_tag in objt:
-                name = abbrs(get_title(objt[name_tag]))
+            for name_tag in ["name", "branch"]:
+                if name_tag in objt:
+                    name = abbrs(get_title(objt[name_tag]))
+
+                    # change likely 'St' to 'Saint'
+                    objt[name_tag] = regex.sub(
+                        r"^(St.?)( .+)$",
+                        r"Saint\2",
+                        name,
+                        flags=regex.IGNORECASE,
+                    )
+
+            for addr_tag in ["addr:street_address", "addr:full"]:
+                if addr_tag in objt:
+                    if all(key in objt for key in {"addr:street", "addr:housenumber"}):
+                        objt.pop(addr_tag, None)
+
+                    # split up ATP-generated address field
+                    address_match = regex.match(
+                        r"([0-9]+)(?:-?([A-Z]+))? ([a-zA-Z .'0-9]+)",
+                        objt[addr_tag],
+                    )
+                    if address_match:
+                        objt["addr:housenumber"] = address_match.group(1)
+                        objt["addr:street"] = (
+                            get_title(address_match.group(3))
+                            .rstrip()
+                            .replace("  ", " ")
+                        )
+                        objt.update(
+                            {"addr:unit": address_match.group(2)}
+                            if address_match.group(2)
+                            else {}
+                        )
+                        objt.pop(addr_tag, None)
+                        objt.pop("addr:full", None)
+                        break
+
+            if "addr:city" in objt:
+                # process upper cased cities
+                objt["addr:city"] = abbrs(get_title(objt["addr:city"]))
+
+            if "addr:street" in objt:
+                street = abbrs(objt["addr:street"])
 
                 # change likely 'St' to 'Saint'
-                objt[name_tag] = regex.sub(
-                    r"^(St.?)( .+)$",
-                    r"Saint\2",
-                    name,
-                    flags=regex.IGNORECASE,
+                street = regex.sub(r"^(St.?)( .+)$", r"Saint\2", street)
+                street = regex.sub(r"St.?( [NESW]\.?[EW]?\.?)?$", r"Street\1", street)
+                suite_match = regex.search(
+                    r"(.+?),? (?:S(?:ui)?te |Uni?t |#|R(?:oo)?m )([A-Z0-9]+)",
+                    street,
                 )
+                if suite_match:
+                    street = suite_match.group(1)
+                    objt["addr:unit"] = suite_match.group(2)
 
-        for addr_tag in ["addr:street_address", "addr:full"]:
-            if addr_tag in objt:
-                if all(key in objt for key in {"addr:street", "addr:housenumber"}):
-                    objt.pop(addr_tag, None)
+                objt["addr:street"] = street
 
-                # split up ATP-generated address field
-                address_match = regex.match(
-                    r"([0-9]+)(?:-?([A-Z]+))? ([a-zA-Z .'0-9]+)",
-                    objt[addr_tag],
+            if "addr:housenumber" in objt:
+                # pull out unit numbers
+                unit_match = regex.search(
+                    r"^([0-9]+)[ -]?([A-Za-z]+)$", objt["addr:housenumber"]
                 )
-                if address_match:
-                    objt["addr:housenumber"] = address_match.group(1)
-                    objt["addr:street"] = abbrs(
-                        get_title(address_match.group(3)).rstrip().replace("  ", " ")
-                    )
+                if unit_match:
+                    objt["addr:housenumber"] = unit_match.group(1)
+                    objt["addr:unit"] = unit_match.group(2)
+
+            for phone_tag in ["phone", "contact:phone", "fax"]:
+                if phone_tag in objt:
+                    # split up multiple phone numbers
                     objt.update(
-                        {"addr:unit": address_match.group(2)}
-                        if address_match.group(2)
+                        {phone_tag: objt[phone_tag].split(";")[0]}
+                        if ";" in objt[phone_tag]
                         else {}
                     )
-                    objt.pop(addr_tag, None)
-                    objt.pop("addr:full", None)
-                    break
 
-        if "addr:city" in objt:
-            # process upper cased cities
-            objt["addr:city"] = abbrs(get_title(objt["addr:city"]))
+                    # format US and Canada phone numbers
+                    phone_valid = regex.search(
+                        r"^\(?(?:\+? ?1?[ -.]*)?(?:\(?([0-9]{3})\)?[ -.]*)([0-9]{3})[ -.]*([0-9]{4})$",
+                        objt[phone_tag],
+                    )
+                    phone_perf = regex.search(
+                        r"^\+1 [0-9]{3}-[0-9]{3}-[0-9]{4}$", objt[phone_tag]
+                    )
+                    objt.update(
+                        {
+                            phone_tag: f"+1 {phone_valid.group(1)}-{phone_valid.group(2)}-{phone_valid.group(3)}"
+                        }
+                        if phone_valid and not phone_perf
+                        else {}
+                    )
 
-        if "addr:street" in objt:
-            street = abbrs(objt["addr:street"])
+            for web_tag in ["url", "website", "contact:website"]:
+                if web_tag in objt:
+                    # remove url tracking parameters
+                    objt[web_tag] = regex.sub(
+                        r"(https?:\/\/[^\s?#]+)(\?)[^#\s]*(utm|cid)[^#\s]*",
+                        r"\1",
+                        objt[web_tag],
+                    ).lower()
 
-            # change likely 'St' to 'Saint'
-            street = regex.sub(r"^(St.?)( .+)$", r"Saint\2", street)
-            street = regex.sub(r"St.?( [NESW]\.?[EW]?\.?)?$", r"Street\1", street)
-            suite_match = regex.search(
-                r"(.+?),? (?:S(?:ui)?te |Uni?t |#|R(?:oo)?m )([A-Z0-9]+)",
-                street,
-            )
-            if suite_match:
-                street = suite_match.group(1)
-                objt["addr:unit"] = suite_match.group(2)
-
-            objt["addr:street"] = street
-
-        if "addr:housenumber" in objt:
-            # pull out unit numbers
-            unit_match = regex.search(
-                r"^([0-9]+)[ -]?([A-Za-z]+)$", objt["addr:housenumber"]
-            )
-            if unit_match:
-                objt["addr:housenumber"] = unit_match.group(1)
-                objt["addr:unit"] = unit_match.group(2)
-
-        for phone_tag in ["phone", "contact:phone", "fax"]:
-            if phone_tag in objt:
-                # split up multiple phone numbers
-                objt.update(
-                    {phone_tag: objt[phone_tag].split(";")[0]}
-                    if ";" in objt[phone_tag]
-                    else {}
+            if "addr:postcode" in objt:
+                # remove extraneous postcode digits
+                objt["addr:postcode"] = regex.sub(
+                    r"([0-9]{5})-?0{4}", r"\1", objt["addr:postcode"]
                 )
 
-                # format US and Canada phone numbers
-                phone_valid = regex.search(
-                    r"^\(?(?:\+? ?1?[ -.]*)?(?:\(?([0-9]{3})\)?[ -.]*)([0-9]{3})[ -.]*([0-9]{4})$",
-                    objt[phone_tag],
-                )
-                phone_perf = regex.search(
-                    r"^\+1 [0-9]{3}-[0-9]{3}-[0-9]{4}$", objt[phone_tag]
-                )
-                objt.update(
-                    {
-                        phone_tag: f"+1 {phone_valid.group(1)}-{phone_valid.group(2)}-{phone_valid.group(3)}"
-                    }
-                    if phone_valid and not phone_perf
-                    else {}
-                )
+            if "ref" in objt:
+                # remove refs that are just websites
+                if regex.match(
+                    r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)",
+                    objt["ref"],
+                ):
+                    objt.pop("ref", None)
 
-        for web_tag in ["url", "website", "contact:website"]:
-            if web_tag in objt:
-                # remove url tracking parameters
-                objt[web_tag] = regex.sub(
-                    r"(https?:\/\/[^\s?#]+)(\?)[^#\s]*(utm|cid)[^#\s]*",
-                    r"\1",
-                    objt[web_tag],
-                ).lower()
+            obj["properties"] = objt
 
-        if "addr:postcode" in objt:
-            # remove extraneous postcode digits
-            objt["addr:postcode"] = regex.sub(
-                r"([0-9]{5})-?0{4}", r"\1", objt["addr:postcode"]
-            )
+        with open(file, "w") as f:
+            json.dump(contents, f)
 
-        if "ref" in objt:
-            # remove refs that are just websites
-            if regex.match(
-                r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)",
-                objt["ref"],
-            ):
-                objt.pop("ref", None)
 
-        obj["properties"] = objt
-
-    with open(file, "w") as f:
-        json.dump(contents, f)
+if __name__ == "__main__":
+    run()
